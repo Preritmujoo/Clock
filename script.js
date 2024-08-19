ba2fad728706b16e227f05300a07cfe252d6b7b1
Prerit Mujoo <95234935+Preritmujoo@users.noreply.github.com>
2024-08-19 23:53:49 +0530

    Update script.js
    Added few changes to the interface

--- script.js
+++ script.js
@@ -7,37 +7,44 @@ setInterval(() => {
   let pm, greet;
 
   if (h < 12) {
-    greet = "Good Morning"
-    pm = "AM"
+    greet = "Good Morning";
+    pm = "AM";
+  } else if (h < 16) {
+    greet = "Good Afternoon";
+    pm = "PM";
+  } else {
+    greet = "Good Evening";
+    pm = "PM";
   }
 
-  else if( h < 16){
-    greet = "Good Afternoon"
-    pm = "PM"
-  }
-
-  else{
-    greet = "Good Evening"
-    pm = "PM"
-  }
-
-  if(h == 0){
+  if (h == 0) {
     h = 12;
-  }
-
-  else if(h > 12){
+  } else if (h > 12) {
     h -= 12;
   }
 
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
-  
+
   let d = a.getDate();
   let M = a.getMonth();
   let Y = a.getFullYear();
 
-  const monthNames = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
   M = monthNames[M];
 
   let p1 = document.getElementById("date");
@@ -46,22 +53,21 @@ setInterval(() => {
     p1.id = "date";
     document.body.append(p1);
   }
-  p1.innerText = `Today is ${d} ${M} ${Y}`;
+  p1.innerHTML = `<p>Today is <span id="spDate">${d} ${M} ${Y}</span></p>`;
 
-  let p2 = document.getElementById("time")
+  let p2 = document.getElementById("time");
   if (!p2) {
     let p2 = document.createElement("p");
     p2.id = "time";
     document.body.append(p2);
   }
-  p2.innerText = `The current time is ${h}:${m}:${s} ${pm}`
+  p2.innerHTML = `<p>And current time is <span id="spTime">${h}:${m}:${s} ${pm}</span></p>`;
 
-
-    let p3 = document.getElementById("greeting")
-    if (!p3) {
-      let p3 = document.createElement("p");
-      p3.id = "greeting";
-      document.body.append(p3);
-    }
-    p3.innerText = `${greet}`
+  let p3 = document.getElementById("greeting");
+  if (!p3) {
+    let p3 = document.createElement("p");
+    p3.id = "greeting";
+    document.body.append(p3);
+  }
+  p3.innerText = `So, ${greet}!`;
 }, 10);
